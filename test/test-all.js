5b1aaa7747f6c4f2c305f8ebd624a6c64c32c7bc
cmfatih <cmuratfatih@gmail.com>
2014-04-26 05:38:09 -0400

    Add unit test

--- test/test-all.js
+++ test/test-all.js
@@ -1,12 +1,26 @@
 // Init reqs
 /* jslint node: true */
+/* global describe: false */
+/* global it: false */
 'use strict';
 
 process.argv.push('-c', 'config/test.json');
 
-var asqsmdb = require('../app/'),
-    utilex  = require('utilex')
-;
+var utilex = require('utilex');
 
 // Tests
-utilex.tidyLog('test-all.js');
+
+// Test for asqsmdb module
+describe('asqsMDB', function() {
+
+  if(!utilex.tidyConfig().error && utilex.tidyConfig().file) {
+    it('should run without any error', function(done) {
+      require('../app/');
+      setTimeout(function() { done(); }, 5000);
+    });
+  } else {
+    it('should run without any error (invalid file!)', function(done) {
+      done();
+    });
+  }
+});
